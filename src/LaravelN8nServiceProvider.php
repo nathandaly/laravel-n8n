9c1d002ac3ddf73c8b25e74e1647d92f4871c97c
Nate <nathandalypretoria@hotmail.co.uk>
2025-08-01 09:54:25 +0100

    :rocket: The start of something productive.

---
+++ src/LaravelN8nServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NateDaly\LaravelN8n;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelN8nServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-n8n.php', 'laravel-n8n'
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/laravel-n8n.php' => config_path('laravel-n8n.php'),
+            ], 'laravel-n8n-config');
+        }
+    }
+}
